230339d3151047c302c2675cd3fca0e8fcefa9b1
SwAurora <rlatjddnr74@gmail.com>
2022-11-27 17:07:27 +0900

    2022-11-27 master(ver1)

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,7 +3,6 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -11,7 +10,7 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원이 넘게 입력해주세요.")
 public class Item
 {
-
+    @NotNull //수정 요구사항 추가
     private Long id;
     @NotBlank
     private String itemName;
@@ -19,7 +18,7 @@ public class Item
     @Range(min = 1000, max = 1000000)
     private Integer price;
     @NotNull
-    @Max(9999)
+    //@Max(9999)
     private Integer quantity;
 
     public Item()
